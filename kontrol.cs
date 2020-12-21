5817846b4dfe60cd14acc4f80dae82bafee9aa6d
FeyzaDuman <feyzaduman_43@hotmail.com>
2020-12-22 00:17:38 +0300

    githup proje-3

---
+++ kontrol.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kontrol : MonoBehaviour {
+
+    public Sprite[] KusSprite;
+    SpriteRenderer spriteRenderer;
+    bool ileriGeriKontrol = true ;
+    int kusSayac = 0;
+    float kusAnimasyonZaman= 0;
+    bool oyunBitti = true;
+    Rigidbody2D fizik;
+    int puan = 0;
+
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        fizik = GetComponent<Rigidbody2D>(); }
+	void Update () {
+        if (Input.GetMouseButtonDown(0)&& oyunBitti)
+        {
+            fizik.velocity = new Vector2(0, 0);
+            fizik.AddForce(new Vector2(0,200));
+        }
+         Animasyon();
+}
+    void Animasyon()
+    {
+        kusAnimasyonZaman += Time.deltaTime;
+        if (kusAnimasyonZaman > 0.1f)
+        {
+           kusAnimasyonZaman = 0;
+            if (ileriGeriKontrol)
+            {
+                spriteRenderer.sprite = KusSprite[kusSayac];
+                kusSayac++;
+                if (kusSayac == KusSprite.Length)
+                {
+                    kusSayac--;
+                    ileriGeriKontrol = false;
+                }
+            }
+            else
+            { kusSayac--;
+                spriteRenderer.sprite = KusSprite[kusSayac];
+                if (kusSayac == 0)
+                { kusSayac++;
+                    ileriGeriKontrol = true;
+                }
+            }
+       }
+    }
+    private void OntriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "engel")
+        {
+           oyunBitti = false;    
+        }
+    }
+
+
+
+}
+
+
---
+++ oyunKontrol.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oyunKontrol : MonoBehaviour {
+
+    public GameObject GokyuzuBir;
+    public GameObject Gokyuzuİki;
+
+    Rigidbody2D fizikBir;
+    Rigidbody2D fizikİki;
+
+    public GameObject engel;
+    public int kacAdetEngel;
+    GameObject[] engeller;
+    float degisimZamani = 0;
+   
+
+    int sayac = 0;
+    void Start () {
+        fizikBir = GokyuzuBir.GetComponent<Rigidbody2D>();
+        fizikİki = Gokyuzuİki.GetComponent<Rigidbody2D>();
+
+
+        fizikBir.velocity = new Vector2(-1.5f,0);
+        fizikİki.velocity = new Vector2(-1.5f, 0);
+
+        
+        engeller = new GameObject[kacAdetEngel];
+
+        for (int i =0;i<engeller.Length;i++)
+        {
+
+            engeller[i] = Instantiate(engel,new Vector2(-20,-20),Quaternion.identity);
+            Rigidbody2D fizikEngel = engeller[i].AddComponent<Rigidbody2D>();
+            fizikEngel.gravityScale = 0;
+            fizikEngel.velocity = new Vector2(-1.5f,0);
+
+
+        }
+
+    }
+	
+	
+	void Update () {
+
+        
+
+        degisimZamani += Time.deltaTime;
+        if (degisimZamani>2f)
+        {
+            degisimZamani = 0;
+            float Yeksenim = Random.Range(-9f,0.4f);
+            engeller[sayac].transform.position = new Vector3(18, Yeksenim);
+            sayac++;
+            if (sayac>=engeller.Length)
+            {
+                sayac = 0;
+            }
+
+
+
+        }
+
+
+
+	}
+}
